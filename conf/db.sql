74d0a4be76c9547ef76fab9b7df5617d45b0b997
DevonZheng <kzwhui@gmail.com>
2016-08-27 09:40:32 +0800

    add stock tick info db table

--- conf/db.sql
+++ conf/db.sql
@@ -1,5 +1,7 @@
 create database `d_finance`;
 
+USE `d_finance`;
+
 CREATE TABLE `t_real_time_info`(
   `c_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `c_name` varchar(128) NOT NULL,
@@ -39,6 +41,39 @@ CREATE TABLE `t_real_time_info`(
   PRIMARY KEY (`c_id`),
   KEY `code` (`c_code`),
   UNIQUE KEY `code_date_time` (`c_date_time`, `c_code`),
+  KEY `modify_time` (`c_modify_time`),
+  KEY `create_time` (`c_create_time`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_stock_tick_info`(
+  `c_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `c_code` varchar(64) NOT NULL,
+  `c_price` float NOT NULL,
+  `c_pchange` float NOT NULL,
+  `c_change` float NOT NULL,
+  `c_volume` bigint(20) unsigned NOT NULL,
+  `c_amount` double NOT NULL,
+  `c_type` varchar(64) NOT NULL,
+  `c_date_time` datetime NOT NULL,
+  `c_modify_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `c_create_time` datetime NOT NULL,
+  PRIMARY KEY (`c_id`),
+  KEY `code` (`c_code`),
+  UNIQUE KEY `code_date_time` (`c_date_time`, `c_code`),
+  KEY `modify_time` (`c_modify_time`),
+  KEY `create_time` (`c_create_time`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_stock_info`(
+  `c_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `c_code` varchar(64) NOT NULL,
+  `c_industry` varchar(64) NOT NULL,
+  `c_area` varchar(64) NOT NULL,
+  `c_concept` varchar(64) NOT NULL,
+  `c_modify_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `c_create_time` datetime NOT NULL,
+  PRIMARY KEY (`c_id`),
+  UNIQUE KEY `code` (`c_code`),
   KEY `modify_time` (`c_modify_time`),
   KEY `create_time` (`c_create_time`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
